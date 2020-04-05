60d0054920e60e6173c9500488210eb0e2c32271
Rajender Dindyal <rajenderdindyal@Rajenders-MacBook-Air.local>
2020-04-05 18:06:17 +0530

    yup

--- src/db/migrations/20200405121857-create-user.js
+++ src/db/migrations/20200405121857-create-user.js
@@ -6,28 +6,31 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
       },
       phone: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+      },
+      authToken: {
+        type: Sequelize.INTEGER,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.dropTable('Users');
-  }
+  },
 };
--- src/db/models/user/user.js
+++ src/db/models/user/user.js
@@ -1,10 +1,17 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phone: DataTypes.INTEGER
-  }, {});
+  const User = sequelize.define(
+    'User',
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      phone: DataTypes.INTEGER,
+      authToken: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {},
+  );
   User.associate = function(models) {
     // associations can be defined here
   };
